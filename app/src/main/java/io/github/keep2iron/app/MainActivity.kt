c60dc6b041caa1a2effdb232feba906d1381b4f2
keep2iron <497881309@qq.com>
2018-10-23 15:13:01 +0800

    Initial commit

---
+++ app/src/main/java/io/github/keep2iron/app/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.github.keep2iron.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.github.keep2iron.pomelo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
