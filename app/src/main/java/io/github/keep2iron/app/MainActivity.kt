9a688a6462ee0d7cc09e05abbbee4dc445b972ac
keep2iron <497881309@qq.com>
2018-10-23 16:21:23 +0800

    修改build文件

--- app/src/main/java/io/github/keep2iron/app/MainActivity.kt
+++ app/src/main/java/io/github/keep2iron/app/MainActivity.kt
@@ -2,12 +2,15 @@ package io.github.keep2iron.app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import io.github.keep2iron.pomelo.R
+//import io.github.keep2iron.network.NetworkManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+//        NetworkManager.Builder
     }
 }
