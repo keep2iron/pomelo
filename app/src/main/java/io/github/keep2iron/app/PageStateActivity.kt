c3ba9d5074c66cf579b086e5a6f2af1d9d006c83
xueshijun <xsj092009>
2019-10-11 14:21:36 +0800

    1.升级versions 2.修复点击事件处理

--- app/src/main/java/io/github/keep2iron/app/PageStateActivity.kt
+++ app/src/main/java/io/github/keep2iron/app/PageStateActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DiffUtil.ItemCallback
 import androidx.recyclerview.widget.RecyclerView
@@ -27,176 +28,196 @@ import io.reactivex.schedulers.Schedulers
 import java.io.IOException
 
 class PageStateActivity : AppCompatActivity(), LoadListener {
-  //    val data = DiffObservableList(object : DiffUtil.ItemCallback<Movie>() {
+    //    val data = DiffObservableList(object : DiffUtil.ItemCallback<Movie>() {
 //        override fun areItemsTheSame(p0: Movie, p1: Movie): Boolean = p0.id == p1.id
 //
 //        override fun areContentsTheSame(p0: Movie, p1: Movie): Boolean = p0 == p1
 //    })
-  val data = AsyncDiffObservableList(object : ItemCallback<Any>() {
-    override fun areItemsTheSame(
-      p0: Any,
-      p1: Any
-    ): Boolean {
-      if (p0 is Movie && p1 is Movie) {
-        return p0.id == p1.id
-      }
-      if (p0 is Recommend && p1 is Recommend) {
-        return p0.id == p1.id
-      }
-      return false
-    }
-
-    @SuppressLint("DiffUtilEquals")
-    override fun areContentsTheSame(
-      p0: Any,
-      p1: Any
-    ): Boolean {
-      if (p0 is Movie && p1 is Movie) {
-        return p1 == p0
-      }
-      if (p0 is Recommend && p1 is Recommend) {
-        return p0 == p1
-      }
-
-      return false
-    }
-  })
-  private val apiService by FindService(ApiService::class.java)
-  private val pageState = PageStateObservable(PageState.LOADING)
-  private lateinit var binder: BaseBinder
-  private var isFirstLoad = true
-  private var isLoadHomeEnd = false
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_page_state)
-
-    ImageLoaderManager.init(application)
-
-    val pageStateLayout = findViewById<PomeloPageStateLayout>(R.id.pageStateLayout)
-    val refreshLayout =
-      findViewById<SwipeRefreshLayout>(R.id.refreshLayout)
-    val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-    pageStateLayout.setPageStateView(PageState.NETWORK_ERROR, R.layout.item_page_error)
-    pageStateLayout.setPageStateReloadListener(PageState.NETWORK_ERROR) { state, view ->
-      pageState.setPageState(PageState.LOADING)
-      binder.load()
-    }
-
-    pageState.setupWithPageStateLayout(pageStateLayout)
-
-    binder = MultipleTypeBinder(data, recyclerView, refreshLayout, true)
-      .addSubAdapter<Movie>(object : AbstractSubAdapter(1, 10) {
-        override fun render(
-          holder: RecyclerViewHolder,
-          position: Int
-        ) {
-          val item = data[position] as Movie
-          holder.itemView.findViewById<TextView>(R.id.tvText).text = item.movieName
-        }
-
-        override fun onInflateLayoutId(
-          parent: ViewGroup,
-          viewType: Int
-        ): Int = R.layout.item_list
-      })
-      .addSubAdapter<Recommend>(object : AbstractSubAdapter(2, 10) {
-        override fun render(
-          holder: RecyclerViewHolder,
-          position: Int
-        ) {
-          val item = data[position] as Recommend
-
-          ImageLoaderManager.getInstance()
-            .showImageView(holder.itemView.findViewById(R.id.imageView), item.recommandImage) {
-              this.isLoadGif = true
-              this.resizeImageWidth = 100
-              this.resizeImageHeight = 100
+    val data = AsyncDiffObservableList(object : ItemCallback<Any>() {
+        override fun areItemsTheSame(
+            p0: Any,
+            p1: Any
+        ): Boolean {
+            if (p0 is Movie && p1 is Movie) {
+                return p0.id == p1.id
             }
+            if (p0 is Recommend && p1 is Recommend) {
+                return p0.id == p1.id
+            }
+            return false
         }
 
-        override fun onInflateLayoutId(
-          parent: ViewGroup,
-          viewType: Int
-        ): Int = R.layout.item_recommend
-      })
-      .setLoadMore(CustomLoadMore(recyclerView))
-      .setLoadListener(this)
-      .bind()
+        @SuppressLint("DiffUtilEquals")
+        override fun areContentsTheSame(
+            p0: Any,
+            p1: Any
+        ): Boolean {
+            if (p0 is Movie && p1 is Movie) {
+                return p1 == p0
+            }
+            if (p0 is Recommend && p1 is Recommend) {
+                return p0 == p1
+            }
 
-    binder.load()
-  }
+            return false
+        }
+    })
+    private val apiService by FindService(ApiService::class.java)
+    private val pageState = PageStateObservable(PageState.LOADING)
+    private lateinit var binder: BaseBinder
+    private var isFirstLoad = true
+    private var isLoadHomeEnd = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page_state)
 
-  override fun onLoad(
-    controller: LoadController,
-    pagerValue: Any,
-    isPullToRefresh: Boolean
-  ) {
-    Logger.d("onLoad........")
-    pageState.setPageState(PageState.ORIGIN)
-    if (isPullToRefresh && controller.isLoadDefault(pagerValue)) {
-      isLoadHomeEnd = false
+        ImageLoaderManager.init(application)
+
+        val pageStateLayout = findViewById<PomeloPageStateLayout>(R.id.pageStateLayout)
+        val refreshLayout =
+            findViewById<SwipeRefreshLayout>(R.id.refreshLayout)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        pageStateLayout.setPageStateView(PageState.NETWORK_ERROR, R.layout.item_page_error)
+        pageStateLayout.setPageStateReloadListener(PageState.NETWORK_ERROR) { state, view ->
+            pageState.setPageState(PageState.LOADING)
+            binder.load()
+        }
+
+        pageState.setupWithPageStateLayout(pageStateLayout)
+
+        binder = MultipleTypeBinder(data, recyclerView, refreshLayout, true)
+            .addSubAdapter<Movie>(object : AbstractSubAdapter(1, 10) {
+
+                init {
+                    setOnItemClickListener { position, view ->
+                        Toast.makeText(view.context, "Movie position : $position", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    setOnItemClickListener(R.id.tvText) { position, view ->
+                        Toast.makeText(view.context, "tvText Movie position : $position", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+
+                override fun render(
+                    holder: RecyclerViewHolder,
+                    position: Int
+                ) {
+                    val item = data[position] as Movie
+                    holder.itemView.findViewById<TextView>(R.id.tvText).text = item.movieName
+                }
+
+                override fun onInflateLayoutId(
+                    parent: ViewGroup,
+                    viewType: Int
+                ): Int = R.layout.item_list
+            })
+            .addSubAdapter<Recommend>(object : AbstractSubAdapter(2, 10) {
+                override fun render(
+                    holder: RecyclerViewHolder,
+                    position: Int
+                ) {
+                    val item = data[position] as Recommend
+
+                    setOnItemClickListener { position, view ->
+                        Toast.makeText(view.context, "Recommend position : $position", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                    ImageLoaderManager.getInstance()
+                        .showImageView(
+                            holder.itemView.findViewById(R.id.imageView),
+                            item.recommandImage
+                        ) {
+                            this.isLoadGif = true
+                            this.resizeImageWidth = 100
+                            this.resizeImageHeight = 100
+                        }
+                }
+
+                override fun onInflateLayoutId(
+                    parent: ViewGroup,
+                    viewType: Int
+                ): Int = R.layout.item_recommend
+            })
+            .setLoadMore(CustomLoadMore(recyclerView))
+            .setLoadListener(this)
+            .bind()
+
+        binder.load()
     }
 
-    if (!isLoadHomeEnd) {
-      apiService.indexHome(controller.pagerValue() as Int)
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .map {
-          it.value
+    override fun onLoad(
+        controller: LoadController,
+        pagerValue: Any,
+        isPullToRefresh: Boolean
+    ) {
+        Logger.d("onLoad........")
+        pageState.setPageState(PageState.ORIGIN)
+        if (isPullToRefresh && controller.isLoadDefault(pagerValue)) {
+            isLoadHomeEnd = false
         }
-        .subscribe(AndroidSubscriber<List<Movie>> {
-          onSuccess = { respList ->
-            if (respList.isEmpty()) {
-              isLoadHomeEnd = true
-              controller.reset()
-            } else {
-              if (controller.isLoadDefault(pagerValue)) {
-                data.update(respList)
-              } else {
-                data.update(data.toMutableList().also {
-                  it.addAll(respList)
+
+        if (!isLoadHomeEnd) {
+            apiService.indexHome(controller.pagerValue() as Int)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map {
+                    it.value
+                }
+                .subscribe(AndroidSubscriber<List<Movie>> {
+                    onSuccess = { respList ->
+                        if (respList.isEmpty()) {
+                            isLoadHomeEnd = true
+                            controller.reset()
+                        } else {
+                            if (controller.isLoadDefault(pagerValue)) {
+                                data.update(respList)
+                            } else {
+                                data.update(data.toMutableList().also {
+                                    it.addAll(respList)
+                                })
+                            }
+                            controller.intInc()
+                        }
+                        controller.loadComplete()
+                        isFirstLoad = false
+                    }
+                    onError = {
+                        controller.loadFailedComplete()
+
+                        if (it is IOException && isFirstLoad) {
+                            pageState.setPageState(PageState.NETWORK_ERROR)
+                        } else {
+                            pageState.setPageState(PageState.LOAD_ERROR)
+                        }
+                    }
                 })
-              }
-              controller.intInc()
-            }
-            controller.loadComplete()
-            isFirstLoad = false
-          }
-          onError = {
-            controller.loadFailedComplete()
-
-            if (it is IOException && isFirstLoad) {
-              pageState.setPageState(PageState.NETWORK_ERROR)
-            } else {
-              pageState.setPageState(PageState.LOAD_ERROR)
-            }
-          }
-        })
-    } else {
-      apiService.indexRecommend(controller.pagerValue() as Int)
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .map {
-          it.value
+        } else {
+            apiService.indexRecommend(controller.pagerValue() as Int)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map {
+                    it.value
+                }
+                .subscribe(AndroidSubscriber<List<Recommend>> {
+                    onSuccess = { respList ->
+                        controller.loadComplete()
+                        if (respList.isEmpty()) {
+                            controller.showLoadMoreEnd()
+                        } else {
+                            data.update(data.toMutableList().also {
+                                it.addAll(respList)
+                            })
+                            controller.intInc()
+                        }
+                    }
+                    onError = {
+                        controller.loadFailedComplete()
+                    }
+                })
         }
-        .subscribe(AndroidSubscriber<List<Recommend>> {
-          onSuccess = { respList ->
-            controller.loadComplete()
-            if (respList.isEmpty()) {
-              controller.showLoadMoreEnd()
-            } else {
-              data.update(data.toMutableList().also {
-                it.addAll(respList)
-              })
-              controller.intInc()
-            }
-          }
-          onError = {
-            controller.loadFailedComplete()
-          }
-        })
     }
-  }
 
-  override fun defaultValue(): Any = 1
+    override fun defaultValue(): Any = 1
 }
--- extra/src/main/java/io/github/keep2iron/pomlo/pager/adapter/AbstractSubAdapter.kt
+++ extra/src/main/java/io/github/keep2iron/pomlo/pager/adapter/AbstractSubAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.annotation.IdRes
 import androidx.annotation.LayoutRes
 import androidx.collection.SparseArrayCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.alibaba.android.vlayout.DelegateAdapter
 import com.alibaba.android.vlayout.LayoutHelper
 import com.alibaba.android.vlayout.layout.LinearLayoutHelper
@@ -14,75 +15,88 @@ import io.github.keep2iron.pomelo.pager.R
 typealias OnItemClickListener = (position: Int, view: View) -> Unit
 
 abstract class AbstractSubAdapter(
-  val viewType: Int,
-  val cacheMaxViewCount: Int
+    val viewType: Int,
+    val cacheMaxViewCount: Int
 ) : DelegateAdapter.Adapter<RecyclerViewHolder>() {
-  override fun onCreateLayoutHelper(): LayoutHelper = LinearLayoutHelper()
-  private var listenerMap = SparseArrayCompat<OnItemClickListener>()
+    override fun onCreateLayoutHelper(): LayoutHelper = LinearLayoutHelper()
+    private var listenerMap = SparseArrayCompat<OnItemClickListener>()
 
-  /**
-   * 获取布局的layout id
-   *
-   * @return layout id
-   */
-  @LayoutRes
-  abstract fun onInflateLayoutId(
-    parent: ViewGroup,
-    viewType: Int
-  ): Int
+    /**
+     * 获取布局的layout id
+     *
+     * @return layout id
+     */
+    @LayoutRes
+    abstract fun onInflateLayoutId(
+        parent: ViewGroup,
+        viewType: Int
+    ): Int
 
-  /**
-   *
-   * @param holder
-   * @param position
-   */
-  abstract fun render(
-    holder: RecyclerViewHolder,
-    position: Int
-  )
+    /**
+     *
+     * @param holder
+     * @param position
+     */
+    abstract fun render(
+        holder: RecyclerViewHolder,
+        position: Int
+    )
 
-  override fun onCreateViewHolder(
-    parent: ViewGroup,
-    viewType: Int
-  ): RecyclerViewHolder {
-    val view = LayoutInflater.from(parent.context.applicationContext)
-      .inflate(onInflateLayoutId(parent, viewType), parent, false)
-    val holder = RecyclerViewHolder(view)
+    protected lateinit var recyclerView: RecyclerView
 
-    for (i in 0 until listenerMap.size()) {
-      val key = listenerMap.keyAt(i)
-      val listener = listenerMap.get(key)
-      if (key == -1 && listener != null) {
-        holder.itemView.setOnClickListener {
-          listener.invoke(holder.getTag(R.id.pomelo_adapter_position), it)
-        }
-      } else if (key > 0 && listener != null) {
-        holder.itemView.setOnClickListener {
-          listener.invoke(holder.getTag(R.id.pomelo_adapter_position), it)
-        }
-      }
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        this.recyclerView = recyclerView
     }
-    return holder
-  }
 
-  /**
-   * [id]不传默认为-1则该listener添加到rootItem下面
-   */
-  fun setOnItemClickListener(@IdRes id: Int = -1, listener: OnItemClickListener) {
-    listenerMap.put(id, listener)
-  }
+    private val internalClickListener = View.OnClickListener {
+        val listener = listenerMap[it.id]
+        val viewHolder = it.getTag(R.id.pomelo_view_holder) as RecyclerViewHolder
+        listener?.invoke(viewHolder.getTag(R.id.pomelo_adapter_position), it)
+    }
 
-  override fun onBindViewHolder(
-    holder: RecyclerViewHolder,
-    position: Int
-  ) {
-    holder.setTag(R.id.pomelo_adapter_position, position)
-    render(holder, position)
-  }
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerViewHolder {
+        val view = LayoutInflater.from(parent.context.applicationContext)
+            .inflate(onInflateLayoutId(parent, viewType), parent, false)
+        val holder = RecyclerViewHolder(view)
 
-  override fun getItemViewType(position: Int): Int {
-    return viewType
-  }
+        for (i in 0 until listenerMap.size()) {
+            val key = listenerMap.keyAt(i)
+            val listener = listenerMap.get(key)
+            if (key == -1 && listener != null) {
+                holder.itemView.setTag(R.id.pomelo_view_holder, holder)
+                holder.itemView.setOnClickListener(internalClickListener)
 
-  override fun getItemCount(): Int = 1
+            } else if (key > 0 && listener != null) {
+                val childView = holder.itemView.findViewById<View>(key)
+                childView.setTag(R.id.pomelo_view_holder, holder)
+                childView.setOnClickListener(internalClickListener)
+            }
+        }
+        return holder
+    }
+
+    /**
+     * [id]不传默认为-1则该listener添加到rootItem下面
+     */
+    fun setOnItemClickListener(@IdRes id: Int = -1, listener: OnItemClickListener) {
+        listenerMap.put(id, listener)
+    }
+
+    override fun onBindViewHolder(
+        holder: RecyclerViewHolder,
+        position: Int
+    ) {
+        holder.setTag(R.id.pomelo_adapter_position, position)
+        render(holder, position)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return viewType
+    }
+
+    override fun getItemCount(): Int = 1
 }
